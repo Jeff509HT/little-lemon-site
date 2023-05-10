424e1fe280df20e8d14503468a39e21a50324a4e
Jeff509HT <voltairejeff898@gmail.com>
2023-05-10 10:23:34 -0600

    Add some tag OGP and some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import Nav from './Nav'
+import Main from './Main'
+import Header from './Header'
+import Footer from './Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Homepage
+     <Header/>
+     <Nav/>
+     <Main/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from './icons_assets/Logo.svg'
+
+const Nav =()=>{
+    return (
+        <div>
+            <img src={logo}></img>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    );
+}
+
+
+export default Nav;
